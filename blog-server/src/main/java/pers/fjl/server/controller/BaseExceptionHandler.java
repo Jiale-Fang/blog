dcf341271701a3acea2cb16adea810d786aee509
方嘉乐 <8109863+fang-jiale@user.noreply.gitee.com>
2021-02-01 09:33:14 +0800

    "全局异常处理"

--- blog-server/src/main/java/pers/fjl/server/controller/BaseExceptionHandler.java
+++ blog-server/src/main/java/pers/fjl/server/controller/BaseExceptionHandler.java
@@ -1,5 +1,6 @@
 package pers.fjl.server.controller;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pers.fjl.common.constant.MessageConstant;
 import pers.fjl.common.entity.Result;
+
+import javax.validation.ConstraintViolationException;
 
 /**
  * 异常处理控制器
@@ -19,31 +22,33 @@ import pers.fjl.common.entity.Result;
 //@Api(value = "全局异常处理模块", description = "全局异常处理的接口信息")
 public class BaseExceptionHandler {
 
-//    @ExceptionHandler(Exception.class)
-//    @ResponseBody
-//    public Result handler(Exception e) {
-//        if (e instanceof RuntimeException) {
-//            return new Result(false, MessageConstant.ERROR, e.getMessage());
-//        }
-//        log.error("Exception:[{}]", e);
-//        return new Result(false, MessageConstant.ERROR, e.getMessage());
-//    }
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result handler(Exception e) {
+        if (e instanceof RuntimeException) {
+            return new Result(false, e.getMessage(), MessageConstant.ERROR);
+        }
+        log.error("Exception:[{}]", e);
+        return new Result(false, e.getMessage(), MessageConstant.ERROR);
+    }
 
-//    @ExceptionHandler(value = {RuntimeException.class})
-//    public Result runtimeHandler(RuntimeException e) {
-//        System.out.println("出现异常："+e.getMessage());
-//        return new Result(false, MessageConstant.LOGINERROR, e.getMessage());
-//    }
-//
-//    @ExceptionHandler(value = {JWTVerificationException.class})
-//    public Result jwtHandler(JWTVerificationException e) {
-//        log.debug(e.getMessage());
-//        return new Result(false, MessageConstant.ACCESSERROR, e.getMessage());
-//    }
-//
-//    @ExceptionHandler(value = {ConstraintViolationException.class})
-//    @ResponseBody
-//    public Result validHandle(ConstraintViolationException e) {
-//        return new Result(false, MessageConstant.ERROR, e.getMessage());
-//    }
+    @ExceptionHandler(value = {RuntimeException.class})
+    @ResponseBody
+    public Result runtimeHandler(RuntimeException e) {
+        System.out.println("出现异常：" + e.getMessage());
+        return new Result(false, e.getMessage(), MessageConstant.LOGINERROR);
+    }
+
+    @ExceptionHandler(value = {JWTVerificationException.class})
+    @ResponseBody
+    public Result jwtHandler(JWTVerificationException e) {
+        log.debug(e.getMessage());
+        return new Result(false, e.getMessage(), MessageConstant.ACCESSERROR);
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    @ResponseBody
+    public Result validHandle(ConstraintViolationException e) {
+        return new Result(false, e.getMessage(), MessageConstant.ERROR);
+    }
 }
