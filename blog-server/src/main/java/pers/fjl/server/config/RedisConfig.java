cb379022a78e8f3ee4ee6f0b97be7df39402c454
方嘉乐 <8109863+fang-jiale@user.noreply.gitee.com>
2021-02-03 19:38:19 +0800

    "关于redis缓存的配置和工具类"

---
+++ blog-server/src/main/java/pers/fjl/server/config/RedisConfig.java
@@ -0,0 +1,89 @@
+package pers.fjl.server.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    /**
+     * 选择redis作为默认缓存工具
+     * 　　  * SpringBoot2.0以上CacheManager配置方式
+     *
+     * @param redisTemplate
+     * @return
+     */
+    @Bean
+    public CacheManager cacheManager(RedisTemplate<String, Object> redisTemplate) {
+        RedisCacheConfiguration defaultCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                // 设置key为String
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getStringSerializer()))
+                // 设置value 为自动转Json的Object
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()))
+                // 不缓存null
+                .disableCachingNullValues()
+                // 缓存数据保存1小时
+                .entryTtl(Duration.ofHours(1));
+        RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder
+                // Redis 连接工厂
+                .fromConnectionFactory(redisTemplate.getConnectionFactory())
+                // 缓存配置
+                .cacheDefaults(defaultCacheConfiguration)
+                // 配置同步修改或删除 put/evict
+                .transactionAware()
+                .build();
+
+//        RedisCacheManager rcm = new RedisCacheManager(redisTemplate);
+        return redisCacheManager;
+    }
+
+
+    /**
+     * retemplate相关配置
+     *
+     * @param factory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        // 配置连接工厂
+        template.setConnectionFactory(factory);
+
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer jacksonSeial = new Jackson2JsonRedisSerializer(Object.class);
+
+        ObjectMapper om = new ObjectMapper();
+        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // 指定序列化输入的类型，类必须是非final修饰的，final修饰的类，比如String,Integer等会跑出异常
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jacksonSeial.setObjectMapper(om);
+
+        // 值采用json序列化
+        template.setValueSerializer(jacksonSeial);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+
+        // 设置hash key 和value序列化模式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(jacksonSeial);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
---
+++ blog-server/src/main/java/pers/fjl/server/utils/RedisUtil.java
@@ -0,0 +1,136 @@
+package pers.fjl.server.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class RedisUtil {
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+    /**
+     * 指定缓存过期时间
+     * @param key  键
+     * @param time 时间 单位秒
+     */
+    public boolean expire(String key,long time) {
+        try {
+            if(time > 0) {
+                redisTemplate.expire(key, time, TimeUnit.SECONDS);
+            }
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    /**
+     * 根据key获取过期时间
+     * @param key 键 不能为空
+     * @return 时间（秒）返回为0 代表永久有效
+     */
+    public long getExpire(String key) {
+        return redisTemplate.getExpire(key, TimeUnit.SECONDS);
+    }
+    /**
+     * 判断key是否存在
+     * @param key 键
+     * @return true 存在  false 不存在
+     */
+    public boolean hasKey(String key) {
+        try {
+            return redisTemplate.hasKey(key);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    /**
+     * 删除缓存
+     * @param key 可以传一个值  或多个
+     */
+    public void del(String...key) {
+        if(key != null && key.length > 0) {
+            if(key.length == 1) {
+                redisTemplate.delete(key[0]);
+            }else {
+                redisTemplate.delete(CollectionUtils.arrayToList(key));
+            }
+        }
+    }
+    //===========================String============================
+    /**
+     * 普通缓存获取
+     * @param key 键
+     * @return 值
+     */
+    public Object get(String key) {
+        return key == null ? null : redisTemplate.opsForValue().get(key);
+    }
+
+    /**
+     * 普通缓存放入
+     * @param key 键
+     * @param value 值
+     * @return true 成功 false 失败
+     */
+    public boolean set (String key,Object value) {
+        try {
+            redisTemplate.opsForValue().set(key, value);
+            return true;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 普通缓存放入并设置是时间
+     * @param key 键
+     * @param value 值
+     * @param time 时间（秒）time如果小于等于0表示无限制
+     * @return true 成功 false 失败
+     */
+    public boolean set (String key,Object value,long time) {
+        try {
+            if(time > 0) {
+                redisTemplate.opsForValue().set(key,value,time,TimeUnit.SECONDS);
+            }else {
+                set(key,value);
+            }
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    /**
+     * 递增
+     * @param key 键
+     * @param delta 要增加多少  必须大于0
+     */
+    public long incr(String key,long delta) {
+        if(delta < 0) {
+            throw new RuntimeException("递增因子必须大于0");
+        }
+        return redisTemplate.opsForValue().increment(key, delta);
+    }
+    /**
+     * 递减
+     * @param key 键
+     * @param delta 要减少多少 必须小于0
+     */
+    public long decr(String key, long delta) {
+        if(delta > 0) {
+            throw new RuntimeException("递减因子必须小于0");
+        }
+        return redisTemplate.opsForValue().increment(key, -delta);
+    }
+}
