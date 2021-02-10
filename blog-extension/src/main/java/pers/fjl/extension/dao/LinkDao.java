cc491a775e208b544c180aa4ea32d5a658b6ca30
方嘉乐 <8109863+fang-jiale@user.noreply.gitee.com>
2021-02-10 16:59:23 +0800

    "友链页面的开发"

---
+++ blog-extension/src/main/java/pers/fjl/extension/dao/LinkDao.java
@@ -0,0 +1,18 @@
+package pers.fjl.extension.dao;
+
+import org.springframework.stereotype.Repository;
+import pers.fjl.extension.po.Link;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author fangjiale
+ * @since 2021-02-08
+ */
+@Repository
+public interface LinkDao extends BaseMapper<Link> {
+
+}
