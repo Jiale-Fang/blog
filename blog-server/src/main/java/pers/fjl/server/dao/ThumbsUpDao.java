7cdefcd03064adab382ea3d681c4044ce8fd13e8
方嘉乐 <8109863+fang-jiale@user.noreply.gitee.com>
2021-02-21 11:34:24 +0800

    "点赞功能的实现"

---
+++ blog-server/src/main/java/pers/fjl/server/dao/ThumbsUpDao.java
@@ -0,0 +1,16 @@
+package pers.fjl.server.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import pers.fjl.common.po.ThumbsUp;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author fangjiale
+ * @since 2021-02-20
+ */
+public interface ThumbsUpDao extends BaseMapper<ThumbsUp> {
+
+}
