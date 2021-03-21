a18467ad1f0a9ac15b45015347ba919df86f9e74
rosie <1626680964@qq.com>
2021-03-21 20:41:00 +0800

    "爬取博客的实体类"

---
+++ blog-article-crawler/src/main/java/pers/fjl/crawler/po/CrawledBlog.java
@@ -0,0 +1,66 @@
+package pers.fjl.crawler.po;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.time.LocalDateTime;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ * <p>
+ * 从CSDN上爬取下来的博客实体类
+ * </p>
+ *
+ * @author fangjiale
+ * @since 2021-03-14
+ */
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode(callSuper = false)
+public class CrawledBlog extends Model<CrawledBlog> {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "blog_id")
+    private Long blogId;
+
+    private String title;
+
+    private String nickname;
+
+    private String content;
+
+    @TableField("`create_time`")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createTime;
+
+    private String firstPicture;
+
+    private Integer thumbs;
+
+    private Integer views;
+
+    private String description;
+
+    private String avatar;
+
+    @Override
+    protected Serializable pkVal() {
+        return this.blogId;
+    }
+
+}
