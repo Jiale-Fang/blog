0fd5cade7755e00e2f65365e495cdab310ca4126
rosie <1626680964@qq.com>
2021-03-21 20:42:12 +0800

    "任务类"

---
+++ blog-article-crawler/src/main/java/pers/fjl/crawler/task/BlogTask.java
@@ -0,0 +1,114 @@
+package pers.fjl.crawler.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pers.fjl.crawler.pipeline.ArticleTextPipeline;
+import pers.fjl.crawler.pipeline.DbPipeline;
+import pers.fjl.crawler.processor.BlogProcessor;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.scheduler.RedisScheduler;
+
+import javax.annotation.Resource;
+
+/**
+ * 博客任务类
+ */
+@Component
+public class BlogTask {
+//
+//    @Resource
+//    private BlogProcessor blogProcessor;
+//
+//    @Resource
+//    private DbPipeline dbPipeline;
+//
+//    @Resource
+//    private ArticleTextPipeline articleTextPipeline;
+//
+//    @Resource
+//    private RedisScheduler redisScheduler;
+//
+//    /**
+//     * 秒 分 时 日 月 星期
+//     */
+//    @Scheduled(cron = "0 0 23 * * ?")
+//    public void javaTask() {
+//        System.out.println("爬取java模块的文章");
+//        Spider spider = Spider.create(blogProcessor);
+//        spider.addUrl("https://www.csdn.net/tags/NtTaIg5sMzYyLWJsb2cO0O0O.html");
+//        articleTextPipeline.setChannelId("java");
+//        spider.addPipeline(articleTextPipeline);
+//        spider.addPipeline(dbPipeline);
+//        spider.setScheduler(redisScheduler);
+//        spider.start();
+//    }
+//
+//    /**
+//     * 秒 分 时 日 月 星期
+//     */
+//    @Scheduled(cron = "0 0 23 * * ?")
+//    public void springTask() {
+//        System.out.println("爬取spring模块的文章");
+//        Spider spider = Spider.create(blogProcessor);
+//        spider.addUrl("https://www.csdn.net/tags/MtTaEg0sMDg2NTAtYmxvZwO0O0OO0O0O.html");
+//        articleTextPipeline.setChannelId("java");
+//        spider.addPipeline(articleTextPipeline);
+//        spider.addPipeline(dbPipeline);
+//        spider.setScheduler(redisScheduler);
+//        spider.start();
+//    }
+//
+//    /**
+//     * 秒 分 时 日 月 星期
+//     */
+//    @Scheduled(cron = "0 0 23 * * ?")
+//    public void mpTask() {
+//        System.out.println("爬取mp模块的文章");
+//        Spider spider = Spider.create(blogProcessor);
+//        spider.addUrl("https://www.csdn.net/tags/MtTaEg0sMjEzMjItYmxvZwO0O0OO0O0O.html");
+//        articleTextPipeline.setChannelId("java");
+//        spider.addPipeline(articleTextPipeline);
+//        spider.addPipeline(dbPipeline);
+//        spider.setScheduler(redisScheduler);
+//        spider.start();
+//    }
+//
+//    /**
+//     * 秒 分 时 日 月 星期
+//     */
+//    @Scheduled(cron = "0 10 23 * * ?")
+//    public void pythonTask() {
+//        System.out.println("爬取pyhon模块的文章");
+//        Spider spider = Spider.create(blogProcessor);
+//        spider.addUrl("https://www.csdn.net/tags/MtjaQg4sNDk0LWJsb2cO0O0O.html");
+//        articleTextPipeline.setChannelId("python");
+//        spider.addPipeline(articleTextPipeline);
+//        spider.addPipeline(dbPipeline);
+//        spider.setScheduler(redisScheduler);
+//        spider.start();
+//    }
+//
+//    @Scheduled(cron = "0 20 23 * * ?")
+//    public void opsTask() {
+//        System.out.println("爬取运维模块的文章");
+//        Spider spider = Spider.create(blogProcessor);
+//        spider.addUrl("https://www.csdn.net/tags/Ntjakg4sNzAwMC1ibG9n.html");
+//        articleTextPipeline.setChannelId("ops");
+//        spider.addPipeline(articleTextPipeline);
+//        spider.addPipeline(dbPipeline);
+//        spider.setScheduler(redisScheduler);
+//        spider.start();
+//    }
+//
+//    @Scheduled(cron = "0 20 23 * * ?")
+//    public void ops2Task() {
+//        System.out.println("爬取运维模块的文章");
+//        Spider spider = Spider.create(blogProcessor);
+//        spider.addUrl("https://www.csdn.net/tags/MtjaQg5sMDY0MC1ibG9n.html");
+//        articleTextPipeline.setChannelId("ops");
+//        spider.addPipeline(articleTextPipeline);
+//        spider.addPipeline(dbPipeline);
+//        spider.setScheduler(redisScheduler);
+//        spider.start();
+//    }
+}
---
+++ blog-article-crawler/src/main/java/pers/fjl/crawler/task/TrainTask.java
@@ -0,0 +1,38 @@
+package pers.fjl.crawler.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pers.fjl.crawler.service.CnnService;
+import pers.fjl.crawler.service.Word2VecService;
+
+import javax.annotation.Resource;
+
+@Component
+public class TrainTask {
+//
+//    @Resource
+//    private Word2VecService word2VecService;
+//
+//    @Resource
+//    private CnnService cnnService;
+//
+//    /**
+//     * 训练模型
+//     */
+//    @Scheduled(cron = "0 56 11 * * ?")
+//    public void trainModel() {
+//        System.out.println("开始合并语料库......");
+//        word2VecService.mergeWord();
+//        System.out.println("合并语料库结束‐‐‐‐‐‐");
+//
+//        System.out.println("开始构建词向量模型");
+//        word2VecService.build();
+//        System.out.println("构建词向量模型结束");
+//
+//        System.out.println("开始构建神经网络卷积模型");
+//        cnnService.build();
+//        System.out.println("构建神经网络卷积模型结束");
+//    }
+}
+
